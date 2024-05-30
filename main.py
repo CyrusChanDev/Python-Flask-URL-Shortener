3eb05860f4d06a471a61b90e2f35d318fcd858ee
CyrusChanDev <memesarchive111@gmail.com>
2024-05-30 15:51:46 -0700

    Implement app logic to use MySQL database instead of volatile memory

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import random
 from flask import Flask, render_template, redirect, request
 import subprocess
 import mysql.connector
-#db = mysql.connector.connect(host="mydb", user="root", password="root", port=3306)
 
 
 app = Flask(__name__)
-shortened_urls = {}
 
 
 def generate_short_url(length=5):
@@ -21,9 +19,17 @@ def index():
         user_long_url = request.form["long_url"]
         simplified_short_url = generate_short_url(length=5)
 
-        while simplified_short_url in shortened_urls:  # if randomized shortened URLs collide, run function again
+        # there shouldn't be a pre-existing duplicate of the randomly generated short url
+        db_cursor.execute(f"SELECT * FROM url_mapping WHERE short_url = '{simplified_short_url}'")
+        result = db_cursor.fetchone()
+        while result:  # if randomized shortened URLs collide, run function again
             simplified_short_url = generate_short_url(length=5)
-        shortened_urls[simplified_short_url] = user_long_url
+
+            db_cursor.execute(f"SELECT * FROM url_mapping WHERE short_url = '{simplified_short_url}'")
+            result = db_cursor.fetchone()
+
+        db_cursor.execute(f"INSERT INTO url_mapping (original_url, short_url) VALUES ('{user_long_url}','{simplified_short_url}')")
+        db_conn.commit()
         return render_template("index.html", short_url=simplified_short_url)
     
     elif request.method == "GET":
@@ -32,14 +38,22 @@ def index():
 
 @app.route("/<simplified_short_url>")
 def redirect_url(simplified_short_url):
-    user_long_url = shortened_urls.get(simplified_short_url)
-    if user_long_url:
-        return redirect(user_long_url)
+
+    db_cursor.execute(f"SELECT original_url FROM url_mapping WHERE short_url = '{simplified_short_url}'")
+    result = db_cursor.fetchone()
+    if result:
+        return redirect(result[0])
     else:
         return "URL does not exist on our server.", 404
 
 
 if __name__ == "__main__":
     subprocess.call(["python", "./database/create_tables_mysql.py"])
+
+    db_conn =  mysql.connector.connect(host="mydb", user="root", password="root", port=3306, database="url_shortener")
+    db_cursor = db_conn.cursor()
+
     # host="0.0.0.0" is necessary because the app is dockerized and 127.0.0.1 is a loopback address
     app.run(host="0.0.0.0", port=9091, debug=True)
+
+    db_conn.close()
